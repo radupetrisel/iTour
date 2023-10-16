96b91cbe205c704160f4eeb985bdb5a9030c7021
Radu Petrisel <radupetrisel@gmail.com>
2023-10-16 21:22:14 +0300

    Add sample data

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,17 +5,40 @@
 //  Created by Radu Petrisel on 16.10.2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    private func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+        
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
     }
 }
 
